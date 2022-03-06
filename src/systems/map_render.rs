a94460799e4786b88f26d15702259e7ff3d65c5c
Sean O'Brien <obriens.eecs@gmail.com>
2022-03-06 18:56:55 -0500

    initial commit

---
+++ src/systems/map_render.rs
@@ -0,0 +1,30 @@
+use crate::prelude::*;
+
+#[system]
+#[read_component(FieldOfView)]
+#[read_component(Player)]
+pub fn map_render(
+    ecs: &SubWorld,
+    #[resource] map: &Map,
+    #[resource] camera: &Camera,
+    #[resource] theme: &Box<dyn MapTheme>,
+) {
+    let mut fov = <&FieldOfView>::query().filter(component::<Player>()); // (1)
+    let mut draw_batch = DrawBatch::new();
+    draw_batch.target(0);
+
+    let player_fov = fov.iter(ecs).nth(0).unwrap();
+
+    for y in camera.top_y..=camera.bottom_y {
+        for x in camera.left_x..camera.right_x {
+            let pt = Point::new(x, y);
+            let offset = Point::new(camera.left_x, camera.top_y);
+            if map.in_bounds(pt) && player_fov.visible_tiles.contains(&pt) {
+                let idx = map_idx(x, y);
+                let glyph = theme.tile_to_render(map.tiles[idx]);
+                draw_batch.set(pt - offset, ColorPair::new(WHITE, BLACK), glyph);
+            }
+        }
+    }
+    draw_batch.submit(0).expect("Batch error");
+}
